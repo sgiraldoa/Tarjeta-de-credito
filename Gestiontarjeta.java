3c088bdda55be758eaa97580ae2ef7b897bece95
sgiraldoa <101825183+sgiraldoa@users.noreply.github.com>
2022-04-05 13:57:11 -0500

    Add files via upload

---
+++ Gestiontarjeta.java
@@ -0,0 +1,18 @@
+package tarjeta;
+
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Santiago
+ */
+public class Gestiontarjeta extends Usuario {
+    public Usuario usuario;
+    public Date fecha;
+}
